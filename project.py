2c6d84f2fdc0985bd3e084e721680ad16ab3b51e
inozemetz <74661405+inozemetz@users.noreply.github.com>
2021-01-20 17:35:04 +0300

    Update project.py

--- project.py
+++ project.py
@@ -28,7 +28,6 @@ def terminate():
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     return image
